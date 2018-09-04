bfb4371c7fc40d48e5b358354bad7df951c31ad8
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 00:51:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-//takeANumber accepts two parameters--num people in line and new persons name.  
+//takeANumber accepts two parameters--line and persons name.  includes message "Welcome, ${name}. You are number ${line} in line"
+
+//nowServing accepts current line of people(katzDeliLine) and returns and removes the next individual in line. When line is empty, return "There is nobody waiting to be served!"
+
+//currentLine accepts line and returns "The line is  currently: dkj"
