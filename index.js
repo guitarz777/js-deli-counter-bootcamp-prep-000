0ff10e017f728e9d024bf772ab3bb6ca4e91dd89
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 00:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function takeANumber (line, name){
 
 //nowServing accepts current line of people(katzDeliLine) and returns and removes the next individual in line. When line is empty, return "There is nobody waiting to be served!"
 
+function nowServing(line){
+  return line.shift()
+}
+
 //currentLine accepts line and returns "The line is  currently: 1. name, 2. name, 3. name ... n. name".  If line is empty, returns "The line is currently empty."
